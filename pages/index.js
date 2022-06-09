2e474a583e4a161510969c20a0be7b2d851e92ac
swheel <swheel33@gmail.com>
2022-06-09 12:26:39 -0400

    init

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import App from '../components/App'
+
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Samuel Wheeler</title>
+      </Head>
+      <App />
+    </div>
+  )
+}
