873741ee41ceb33fb5a0bd43fdbda43899d91b8b
swheel <swheel33@gmail.com>
2022-06-09 13:58:09 -0400

    update text

--- components/Home.jsx
+++ components/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <Box p='2rem' w={['100%','50%']}>
             <Text>Welcome to my website! My name is Samuel Wheeler and I'm a software developer focused on Web3 and decentralized finance (Defi). 
-                You can learn more about my career trajectory in the About section and check out my portfolio in the Projects section.
+                You can learn more about my career trajectory and interests in the About section, and check out my portfolio in the Projects section.
             </Text>
         </Box>
     )
